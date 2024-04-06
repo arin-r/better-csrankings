71a4bdc052a02964bfe9533b4e28fa82000f8b89
Harsh <harshbhatia1605@gmail.com>
2024-04-07 01:18:23 +0530

    changed title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,15 @@
-import { cn } from "@/lib/utils";
-import "./globals.css";
-import type { Metadata } from "next";
-import { Analytics } from "@vercel/analytics/react";
+import { cn } from '@/lib/utils';
+import { Analytics } from '@vercel/analytics/react';
+import type { Metadata } from 'next';
+import './globals.css';
 
 // import Drawer from "@/components/Drawer";
-import { fontSans } from "@/lib/fonts";
-import React from "react";
-import { Separator } from "@/components/ui/separator";
+import { Separator } from '@/components/ui/separator';
+import { fontSans } from '@/lib/fonts';
+import React from 'react';
 
 export const metadata: Metadata = {
-  // title: "BPGC Everyone",
-  description: "A website to view BITS-GOA student information",
+  description: 'A website to view Indian CSRankings',
 };
 
 export default function RootLayout({
@@ -19,22 +18,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang='en' suppressHydrationWarning>
       <head>
-        <title>BPGC Everyone</title>
-        <link rel="icon" href="/magnifying-glass.svg" />
+        <title>CSRankings but Better</title>
+        <link rel='icon' href='/magnifying-glass.svg' />
       </head>
       <body
         className={cn(
-          "dark:bg-stone-950 bg-white relative flex min-h-screen w-full flex-col justify-center scroll-smooth bg-background font-sans antialiased",
+          'dark:bg-stone-950 bg-white relative flex min-h-screen w-full flex-col justify-center scroll-smooth bg-background font-sans antialiased',
           fontSans.variable,
-          "dark"
+          'dark'
         )}
       >
-        <div className="flex min-h-screen flex-col items-center px-3 pt-8">
+        <div className='flex min-h-screen flex-col items-center px-3 pt-8'>
           {children}
         </div>
-        <Separator className="mt-3 w-full" />
+        <Separator className='mt-3 w-full' />
         <Analytics />
       </body>
     </html>
