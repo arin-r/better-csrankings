c4de9c794e07e7c1ca09d2dda35a03ecdd9bdc29
arin-r <arinsroday@gmail.com>
2024-04-07 14:40:24 +0530

    fetched and parsed data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,81 @@
-"use client"
+"use client";
 import { Button, buttonVariants } from "@/components/ui/button";
 import Link from "next/link";
 import axios from "axios";
+import {
+  PublicationWithoutAffiliation,
+  PublicationsByAffiliation,
+  PublicationsByYear,
+} from "@/lib/types";
+
+function countPublications(publications: PublicationsByYear): number {
+  let totalCount = 0;
+  for (const pubs of Object.values(publications)) {
+    // Soometimes pubs is not an array, I encountered a value "true" for some reason. 
+    // Hence I added this check to make sure it is an array before iterating over it.
+    if (pubs.length != undefined) {
+      totalCount += pubs.length;
+    }
+  }
+  return totalCount;
+}
+
+function convertToPublicationsByAffiliation(
+  publicationsByYear: PublicationsByYear
+): PublicationsByAffiliation {
+  const publicationsByAffiliation: PublicationsByAffiliation = {};
+
+  for (const [year, pubs] of Object.entries(publicationsByYear)) {
+    // Soometimes pubs is not an array, I encountered a value "true" for some reason. 
+    // Hence I added this check to make sure it is an array before iterating over it.
+    if (pubs.length != undefined) {
+      // console.log("pubs = ", pubs);
+      pubs.forEach((pub) => {
+        const { author, affiliation, points, FOR } = pub;
+        const publicationWithoutAffiliation: PublicationWithoutAffiliation = {
+          author,
+          points,
+          FOR,
+          year,
+        };
+
+        if (!publicationsByAffiliation[affiliation]) {
+          publicationsByAffiliation[affiliation] = [
+            publicationWithoutAffiliation,
+          ];
+        } else {
+          publicationsByAffiliation[affiliation].push(
+            publicationWithoutAffiliation
+          );
+        }
+      });
+    }
+  }
+
+  return publicationsByAffiliation;
+}
 
 const Home = () => {
   return (
     <div>
-      <Link href="/home" className={buttonVariants({
-      })}>
+      <Link href="/home" className={buttonVariants({})}>
         Go to Main Page
       </Link>
+      <Button
+        onClick={async () => {
+          const response = await axios.get("/api/ranking-data");
+          const data = response.data as PublicationsByYear;
+          console.log("data = ", data["2020"][0]);
+          const publicationsByAffiliation =
+            convertToPublicationsByAffiliation(data);
+          console.log(
+            "publicationsByAffiliation = ",
+            publicationsByAffiliation
+          );
+        }}
+      >
+        Fetch Data
+      </Button>
     </div>
   );
 };
---
+++ src/lib/types.ts
@@ -0,0 +1,21 @@
+export interface Publication {
+    author: string;
+    affiliation: string;
+    points: number;
+    FOR: string[];
+}
+
+export interface PublicationsByYear {
+    [year: string]: Publication[];
+}
+
+export interface PublicationWithoutAffiliation {
+    author: string;
+    points: number;
+    FOR: string[];
+    year: string;
+}
+
+export interface PublicationsByAffiliation {
+    [affiliation: string]: PublicationWithoutAffiliation[];
+}
