282a1209a68229f45dc538207d75c8ba6c41ce3f
arin-r <arinsroday@gmail.com>
2024-04-07 18:27:52 +0530

    minor UI issues #2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,27 @@ import Link from "next/link";
 const Home = () => {
   return (
     <div>
-      <Link href="/home" className={buttonVariants({})}>
-        Go to Main Page
-      </Link>
+      <div className="w-full h-screen flex items-center justify-center p-6">
+        <div className="max-w-md text-center mb-6 space-y-4">
+          <h1 className="text-4xl font-bold tracking-tighter">
+            CS Rankings
+          </h1>
+          <h1 className="text-4xl font-bold tracking-tighter">
+            But
+          </h1>
+          <h1 className="text-4xl font-bold tracking-tighter">
+            Better
+          </h1>
+          <Link
+          className={buttonVariants({
+            className: "w-[20rem] mt-4",
+          })}
+            href="/home"
+          >
+            Home Page
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
